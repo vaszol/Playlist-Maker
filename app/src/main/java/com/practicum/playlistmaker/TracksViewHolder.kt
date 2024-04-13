aaa425e6912339519caa5337ad4a6902d9fb45b8
vasiliy <vaszol@mail.ru>
2024-04-13 11:51:55 +0300

    Спринт 10/26 → Тема 4/6: Adapter → Урок 2/3 fix

--- app/src/main/java/com/practicum/playlistmaker/TracksViewHolder.kt
+++ app/src/main/java/com/practicum/playlistmaker/TracksViewHolder.kt
@@ -1,10 +1,13 @@
 package com.practicum.playlistmaker
 
+import android.content.Context
+import android.util.TypedValue
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
 class TracksViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
@@ -16,12 +19,22 @@ class TracksViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     fun bind(model: Track) {
         Glide.with(trackImg)
             .load(model.artworkUrl100)
+            .placeholder(R.drawable.ic_music)
             .fitCenter()
             .centerCrop()
+            .transform(RoundedCorners(dpToPx(2F, trackImg.context)))
             .into(trackImg)
         trackName.text = model.trackName
         trackArtist.text = model.artistName
         trackTime.text = model.trackTime
+    }
+
+    private fun dpToPx(dp: Float, context: Context): Int {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            dp,
+            context.resources.displayMetrics
+        ).toInt()
     }
 
 }
