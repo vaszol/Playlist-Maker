9b1140e73dcb9ebff01dda30ed6a18eead675dce
vasiliy <vaszol@mail.ru>
2024-07-30 18:56:21 +0300

    Спринт 16/26 → Тема 5/5: Сдаём задачу спринта №16 на ревью → Урок 1/3 fix crash

--- app/src/main/java/com/practicum/playlistmaker/data/network/ItunesApiClient.kt
+++ app/src/main/java/com/practicum/playlistmaker/data/network/ItunesApiClient.kt
@@ -8,6 +8,7 @@ import com.practicum.playlistmaker.data.dto.Response
 import com.practicum.playlistmaker.data.dto.TrackRequest
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.IOException
 
 class ItunesApiClient(private val context: Context) : NetworkClient {
     private val itunesBaseUrl = "https://itunes.apple.com"
@@ -20,14 +21,18 @@ class ItunesApiClient(private val context: Context) : NetworkClient {
     private val imdbService = retrofit.create(ItunesApi::class.java)
 
     override fun doRequest(dto: Any): Response {
-        if (!isConnected()) {
-            return Response().apply { resultCode = -1 }
-        }
-        if (dto is TrackRequest) {
-            val resp = imdbService.search(dto.expression).execute()
-            val body = resp.body() ?: Response()
-            return body.apply { resultCode = resp.code() }
-        } else {
+        try {
+            if (!isConnected()) {
+                return Response().apply { resultCode = -1 }
+            }
+            if (dto is TrackRequest) {
+                val resp = imdbService.search(dto.expression).execute()
+                val body = resp.body() ?: Response()
+                return body.apply { resultCode = resp.code() }
+            } else {
+                return Response().apply { resultCode = 400 }
+            }
+        } catch (exception: IOException) {
             return Response().apply { resultCode = 400 }
         }
     }
